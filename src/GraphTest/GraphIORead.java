739806a9146c09b886def5b7acd205e51efc1921
tungcr7 <tunga10cvp@gmail.com>
2018-10-22 15:37:19 +0200

    ...

--- src/GraphTest/GraphIORead.java
+++ src/GraphTest/GraphIORead.java
@@ -1,5 +1,8 @@
 package Graphtest;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.MultiGraph;
+
 import java.io.*;
 
 public class GraphIORead {
@@ -8,13 +11,27 @@ public class GraphIORead {
         FileReader file = new FileReader("graph01.gka");
         BufferedReader br = new BufferedReader(file);
 
+        Graph toLoad = new MultiGraph("newLoadedGraph");
         String zeile;
 
 
         while( (zeile = br.readLine()) != null )
         {
-            String graph = new String(zeile.getBytes(),"UTF-8");
-            System.out.println(graph);
+            //String graph = new String(zeile.getBytes(),"UTF-8");
+            String[] currentComponents = zeile.split(";");
+
+            for(String component : currentComponents){
+                if(component.contains("--")) {
+                    String[] extractedNodes = component.split("--");
+                    toLoad.addEdge(extractedNodes[0] + extractedNodes[1], extractedNodes[0], extractedNodes[1]);
+                }
+                else if(component.contains("->")){
+                    String[] extractedNodes = component.split("->");
+                    toLoad.addEdge(extractedNodes[0] + extractedNodes[1], extractedNodes[0], extractedNodes[1]);
+
+                }
+            }
+            System.out.println(zeile);
         }
 
         br.close();
