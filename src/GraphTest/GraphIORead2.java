cbc19d6fd78b8a7894ca71ed986e75cc415d5c2d
tungcr7 <tunga10cvp@gmail.com>
2018-10-31 19:11:16 +0100

    ReadFile neu schreiben, läuft :)

--- src/GraphTest/GraphIORead2.java
+++ src/GraphTest/GraphIORead2.java
@@ -1,102 +1,104 @@
-package GraphTest;
+package Graph;
 
-import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.graph.implementations.SingleGraph;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class GraphIORead2 {
-    /**
-     * Jede Zeilen von den Graphen(line) werden eingelesen
-     * @param line sind die Zeile, die aus den Dateien eingelesen werden
-     */
 
-    public static  graphLine(String line){
-        GraphLine graphLine = new GraphLine();
+    public static void main(String[] args) throws IOException
+    {
 
-        Graph toLoad = new MultiGraph("newLoadedGraph");
+        fileParse();
 
-        FileReader file = new FileReader("graph01.gka");
+    }
+
+    public static void fileParse() throws IOException {
+
+        //
+        Graph graph = new MultiGraph("newLoadedGraph");
+
+        FileReader file = new FileReader("gka-Dateien/graph03.gka");
         BufferedReader br = new BufferedReader(file);
 
-        line = br.readLine();
+        String line;
+        //jede Zeile einlesen
+        while(( line = br.readLine()) != null) {
 
-        String regex = "";
+            // löscht ; für alle Zeilen
+            line = line.replace(";","");
 
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(line);
+            // prüf ob das -> gibt
+            if (line.contains("->")) {
 
-        //jede Zeile prüfen
-        while (matcher.find()){
+                // split ->
+                String[] nodes = line.split(" -> ");
 
-            //wenn die Zeile lerr ist
-            if (graphLine.getNodeSource() == null){
-                System.out.println("Zeile " + line + " hat keinen Graph");
+                // prüf ob der Knote1 vorhanden ist
+                if (graph.getNode(nodes[0]) == null) {
+                    graph.addNode(nodes[0]);
+                }
+
+                // prüf ob der Knote2 vorhanden ist
+                if (graph.getNode(nodes[1]) == null) {
+                    if (nodes[1].contains(":")) {
+                        String[] nodes1 = nodes[1].split(" : ");
+                        if (graph.getNode(nodes1[0]) == null) {
+                            // System.out.println(nodes1[0]);
+                            graph.addNode(nodes1[0]);
+                            graph.addEdge(nodes[0] + nodes1[0], nodes[0], nodes1[0], true).setAttribute("weight", nodes1[1]);
+                        }
+                    }
+                    else {
+                        graph.addNode(nodes[1]);
+                        graph.addEdge(nodes[0] + nodes[1],nodes[0],nodes[1],true);
+                    }
+                }
+                // add to Graph
+                graph.addEdge(nodes[0] + nodes[1],nodes[0],nodes[1],true);
             }
 
-            //wenn die Zeile "->" hat, das heißt gerichter Graph
-            if (line.contains("->")){
-                //erste Knote wird in gruppe 1 gespeichert
-                if(matcher.group(1) != null){
-                    graphLine.setNodeSource(matcher.group(1));
-                }
-                // -> wird in gruppe 2 gespeichert
-                if(matcher.group(2) != null){
-                    graphLine.setDirected(matcher.group(2));
-                }
-                //zweite Konte wird in gruppe 3 gespeichert
-                if(matcher.group(3) != null){
-                    graphLine.setNodeTarget(matcher.group(3));
-                }
-                //edgegewicht wird in gruppe 4 gespeichert
-                if(matcher.group(4) != null){
-                    graphLine.setWeight(Integer.parseInt(matcher.group(4));
-                }
-                //name wird in gruppe 5 gespeichert
-                if(matcher.group(5) != null){
-                    graphLine.setName(matcher.group(5));
-                }
-                // adden die Zeile in den Graph
-                toLoad.addEdge(matcher.group(1) +  matcher.group(3), matcher.group(1),matcher.group(3));
-            }
+            if (line.contains("--")) {
+                String[] nodes = line.split(" -- ");
 
-            //wenn die Zeile "--" hat, das heißt ungerichter Graph
-            else if (line.contains("--")){
-                //erste Knote wird in gruppe 1 gespeichert
-                if(matcher.group(1) != null){
-                    graphLine.setNodeSource(matcher.group(1));
+                if (graph.getNode(nodes[0]) == null) {
+
+                    graph.addNode(nodes[0]);
                 }
-                //zwite Knote wird in gruppe 3 gespeichert
-                if(matcher.group(3) != null){
-                    graphLine.setNodeTarget(matcher.group(3));
+                if (graph.getNode(nodes[1]) == null) {
+                    if (nodes[1].contains(":")) {
+                        String[] nodes1 = nodes[1].split(" : ");
+                        if (graph.getNode(nodes1[0]) == null) {
+                            // System.out.println(nodes1[0]);
+                            graph.addNode(nodes1[0]);
+                            graph.addEdge(nodes[0] + nodes1[0], nodes[0], nodes1[0], false).setAttribute("weight", nodes1[1]);
+                        }
+                    }
+                    else {
+                        graph.addNode(nodes[1]);
+                        graph.addEdge(nodes[0] + nodes[1],nodes[0],nodes[1],false);
+                    }
                 }
-                //Edgegewicht wird in gruppe 4 gespeichert
-                if(matcher.group(4) != null){
-                    graphLine.setWeight(Integer.parseInt(matcher.group(4));
-                }
-                //name wird in gruppe 5 gespeichert
-                if(matcher.group(5) != null){
-                    graphLine.setName(matcher.group(5));
-                }
-                // add die Zeile in den Graph
-                toLoad.addEdge(matcher.group(1) +  matcher.group(3), matcher.group(1),matcher.group(3));
+
 
             }
-            // wenn es einzelne Konte ist
-            else {
-                toLoad.addEdge(matcher.group(1), matcher.group(1), null);
+
+            // einzelner Knote
+            if (line.length() == 1){
+                graph.addNode(line);
             }
+
+            System.out.println(line);
 
         }
-        return graphLine;
-        br.close();
+        graph.display();
     }
+
 
 
 
