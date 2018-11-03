946adb3a754edffa669e8cbb81c5cf82b6788e4b
tungcr7 <tunga10cvp@gmail.com>
2018-11-03 11:47:45 +0100

    ReadFileklasse fertig

--- src/Graph/ReadFile.java
+++ src/Graph/ReadFile.java
@@ -1,105 +1,125 @@
-package Graph;
-
 import org.graphstream.graph.Graph;
-import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.graph.implementations.SingleGraph;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ReadFile {
 
-    public static void main(String[] args) throws IOException
-    {
+    final static File file = new File("gka-Dateien/graph06.gka");
 
-        fileParse();
+    public static void main(final String[] args) {
+        readFile();
+        //graphStream();
 
     }
 
-    public static void fileParse() throws IOException {
-
-        //
-        Graph graph = new MultiGraph("newLoadedGraph");
-
-        FileReader file = new FileReader("gka-Dateien/graph03.gka");
-        BufferedReader br = new BufferedReader(file);
-
+    private static void readFile() {
         String line;
-        //jede Zeile einlesen
-        while(( line = br.readLine()) != null) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            Graph graph = new SingleGraph("Graph01");
 
-            // löscht ; für alle Zeilen
-            line = line.replace(";","");
+            while ((line = br.readLine()) != null) {
+                String ertsePattern =
+                        "^(\\w+)(?:\\s*(->|--)\\s*(\\w+)\\s*(?:\\(\\s*(\\w+)\\s*\\)\\s*)?(?::\\s*(\\d+)\\s*)?)?;";
+                String zweitePattern = "^\\s*(\\w+\\d*)\\s*([\\-\\-\\>]*)\\s*(\\w+\\d*)*\\s*[:]*\\s*(\\d*)";
 
-            // prüf ob das -> gibt
-            if (line.contains("->")) {
+                Pattern pattern = Pattern.compile(ertsePattern);
+                Matcher matcher = pattern.matcher(line);
 
-                // split ->
-                String[] nodes = line.split(" -> ");
-
-                // prüf ob der Knote1 vorhanden ist
-                if (graph.getNode(nodes[0]) == null) {
-                    graph.addNode(nodes[0]);
-                }
-
-                // prüf ob der Knote2 vorhanden ist
-                if (graph.getNode(nodes[1]) == null) {
-                    if (nodes[1].contains(":")) {
-                        String[] nodes1 = nodes[1].split(" : ");
-                        if (graph.getNode(nodes1[0]) == null) {
-                            // System.out.println(nodes1[0]);
-                            graph.addNode(nodes1[0]);
-                            graph.addEdge(nodes[0] + nodes1[0], nodes[0], nodes1[0], true).setAttribute("weight", nodes1[1]);
-                        }
-                    }
-                    else {
-                        graph.addNode(nodes[1]);
-                        graph.addEdge(nodes[0] + nodes[1],nodes[0],nodes[1],true);
-                    }
-                }
-                // add to Graph
-                graph.addEdge(nodes[0] + nodes[1],nodes[0],nodes[1],true);
-            }
-
-            if (line.contains("--")) {
-                String[] nodes = line.split(" -- ");
-
-                if (graph.getNode(nodes[0]) == null) {
-
-                    graph.addNode(nodes[0]);
-                }
-                if (graph.getNode(nodes[1]) == null) {
-                    if (nodes[1].contains(":")) {
-                        String[] nodes1 = nodes[1].split(" : ");
-                        if (graph.getNode(nodes1[0]) == null) {
-                            // System.out.println(nodes1[0]);
-                            graph.addNode(nodes1[0]);
-                            graph.addEdge(nodes[0] + nodes1[0], nodes[0], nodes1[0], false).setAttribute("weight", nodes1[1]);
-                        }
-                    }
-                    else {
-                        graph.addNode(nodes[1]);
-                        graph.addEdge(nodes[0] + nodes[1],nodes[0],nodes[1],false);
-                    }
-                }
-
+                //Lese und zeichne Graphen
+                showGraph(graph, matcher);
 
             }
+            graph.display();
 
-            // einzelner Knote
-            if (line.length() == 1){
-                graph.addNode(line);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void showGraph(Graph graph, Matcher matcher) {
+        while (matcher.find()) {
+            //Auslesen der einzelnen Gruppen
+            final String node1 = matcher.group(1);
+            final String node2 = matcher.group(3);
+            final String edge = matcher.group(2);
+            final String weight = matcher.group(5);
+            final String edgeName = matcher.group(4); // TODO!!!
+
+            System.out.println("Node1: " + node1);
+            System.out.println("Edge: " + edge);
+            System.out.println("Node2: " + node2);
+            System.out.println("EdgeWeight: " + weight);
+            System.out.println("EdgeName: " + edgeName);
+
+            // Knoten 1 hinzufügen, wenn:
+            // - es nicht null ist
+            // - es kein Leerstring ist
+            // - der Knoten nicht schon in dem Graphen ist
+            if (node1 != null &&
+                    !"".equals(node1) &&
+                    graph.getNode(node1) == null) {
+                graph.addNode(node1);
             }
 
-            System.out.println(line);
+            // Knoten 2 hinzufügen, wenn:
+            // - es nicht null ist
+            // - es kein Leerstring ist
+            // - der Knoten nicht schon in dem Graphen ist
+            if (node2 != null &&
+                    !"".equals(node2) &&
+                    graph.getNode(node2) == null) {
+                graph.addNode(node2);
+            }
+
+            // Kante hinzufügen, wenn:
+            // - es nicht null ist
+            // - es kein Leerstring ist
+            // - die Kante nicht schon in dem Graphen ist
+            // - die Kante nicht schon in dem Graphen ist, aber rückwärts gerichtet
+            // - der zweite Knoten nicht null ist
+            if (edge != null &&
+                    !"".equals(edge) &&
+                    graph.getEdge(node1 + node2) == null &&
+                    graph.getEdge(node2 + node1) == null &&
+                    graph.getNode(node2) != null) {
+
+                graph.addEdge(
+                        node1 + node2,
+                        node1,
+                        node2,
+                        edge.equals("->")
+                );
+
+                // Gewicht hinzufügen, wenn:
+                // - es nicht null ist
+                // - es kein Leerstring ist
+                if (weight != null &&
+                        !"".equals(weight)) {
+                    graph.getEdge(node1 + node2).addAttribute("ui.label", weight);
+                }
+            }
 
         }
-        graph.display();
+        //Node Namen
+        graph.getEachNode().forEach(node -> node.addAttribute("ui.label", node.getId()));
     }
 
+    private static void showGroups(Matcher matcher) {
 
-
+        while (matcher.find()) {
+            System.out.println("----------------------------");
+            for (int i = 1; i < matcher.groupCount(); i++) {
+                System.out.println("Group " + i + ": " + matcher.group(i));
+            }
+        }
+    }
 
 }
