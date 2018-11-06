7beb9c6b6a7e84af0627f35905d857d698157c75
tungcr7 <tunga10cvp@gmail.com>
2018-11-06 16:09:50 +0100

    Kommentar hinzugefügt, Schönheit für Graphen implementiert

--- src/Graph/ReadFile.java
+++ src/Graph/ReadFile.java
@@ -1,3 +1,5 @@
+package Graph;
+
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 
@@ -5,35 +7,64 @@ import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Klasse ReadFile fürs Einlesen einer Datei und zeichne den Graphen
+ * Die ReadFile liest jeder Zeile der Dateien und sortiert in die Liste mit Hilfe der Regulär Ausdrücke
+ *
+ *
+ * DIRECTED
+ * <name node1>[ -> <name node2>][(edge name)][: <edgeweight>];
+ * UNDIRECTED
+ * <name node1>[ -- <name node2>][(edge name)][: <edgeweight>];
+ *
+ * @author Ngo Thanh Tung PC
+ *
+ */
 public class ReadFile {
 
-    final static File file = new File("gka-Dateien/graph06.gka");
+
 
     public static void main(final String[] args) {
-        readFile();
-        //graphStream();
+
+        String filename = "graph01.gka";
+        readFile(filename);
 
     }
 
-    private static void readFile() {
+    /**
+
+    **/
+    private static void readFile(String filename) {
+        String filePath = new String("gka-Dateien/" + filename);
+        File file = new File(filePath);
+
+
         String line;
+
+
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(file));
-            Graph graph = new SingleGraph("Graph01");
 
+            //erstellt ein Graph
+            Graph graph = new SingleGraph("GraphStream");
+
+            //liest jeder Zeile ein
             while ((line = br.readLine()) != null) {
-                String ertsePattern =
-                        "^(\\w+)(?:\\s*(->|--)\\s*(\\w+)\\s*(?:\\(\\s*(\\w+)\\s*\\)\\s*)?(?::\\s*(\\d+)\\s*)?)?;";
-                String zweitePattern = "^\\s*(\\w+\\d*)\\s*([\\-\\-\\>]*)\\s*(\\w+\\d*)*\\s*[:]*\\s*(\\d*)";
 
-                Pattern pattern = Pattern.compile(ertsePattern);
+                //regex formatiert jeder Zeile
+                String ersteRegex =
+                        "^([\\wÄäÖöÜüß]+)(?:\\s*(->|--)\\s*([\\wÄäÖöÜüß]+)\\s*(?:\\(\\s*([\\wÄäÖöÜüß]+)\\s*\\)\\s*)?(?::\\s*(\\d+)\\s*)?)?;";
+                String zweiteRegex = "^\\s*(\\w+\\d*)\\s*([\\-\\-\\>]*)\\s*(\\w+\\d*)*\\s*[:]*\\s*(\\d*)";
+
+                Pattern pattern = Pattern.compile(ersteRegex);
                 Matcher matcher = pattern.matcher(line);
 
                 //Lese und zeichne Graphen
                 showGraph(graph, matcher);
-
             }
+
+            //zeichnet Graph
             graph.display();
 
         } catch (FileNotFoundException e) {
@@ -45,6 +76,8 @@ public class ReadFile {
     }
 
     private static void showGraph(Graph graph, Matcher matcher) {
+
+        //suche alle Teile of Dateien
         while (matcher.find()) {
             //Auslesen der einzelnen Gruppen
             final String node1 = matcher.group(1);
@@ -53,12 +86,20 @@ public class ReadFile {
             final String weight = matcher.group(5);
             final String edgeName = matcher.group(4); // TODO!!!
 
-            System.out.println("Node1: " + node1);
-            System.out.println("Edge: " + edge);
-            System.out.println("Node2: " + node2);
-            System.out.println("EdgeWeight: " + weight);
-            System.out.println("EdgeName: " + edgeName);
+//            System.out.println("Node1: " + node1);
+//            System.out.println("Edge: " + edge);
+//            System.out.println("Node2: " + node2);
+//            System.out.println("EdgeWeight: " + weight);
+//            System.out.println("EdgeName: " + edgeName);
 
+            // drück jede Zeile nach dem Sortieren aus
+
+            System.out.println(graph.getEdgeCount());
+            System.out.println("----------------------------");
+            for (int i = 0; i <= matcher.groupCount(); i++) {
+                System.out.println("Group " + i + ": " + matcher.group(i));
+            }
+            //System.out.println(matcher.groupCount());
             // Knoten 1 hinzufügen, wenn:
             // - es nicht null ist
             // - es kein Leerstring ist
@@ -88,7 +129,7 @@ public class ReadFile {
             if (edge != null &&
                     !"".equals(edge) &&
                     graph.getEdge(node1 + node2) == null &&
-                    graph.getEdge(node2 + node1) == null &&
+                    graph.getEdge(node2 + node1) == null  &&
                     graph.getNode(node2) != null) {
 
                 graph.addEdge(
@@ -105,21 +146,82 @@ public class ReadFile {
                         !"".equals(weight)) {
                     graph.getEdge(node1 + node2).addAttribute("ui.label", weight);
                 }
+
             }
 
         }
         //Node Namen
         graph.getEachNode().forEach(node -> node.addAttribute("ui.label", node.getId()));
+
+        graph.addAttribute("ui.stylesheet", styleSheet);
+        //graph.addAttribute("ui.stylesheet", "graph { fill-color: yellow; }");
+        graph.addAttribute("ui.quality");
+        graph.addAttribute("ui.antialias");
+       // graph.addAttribute("ui.screenshot", "heart.jpg");
+
+
     }
 
-    private static void showGroups(Matcher matcher) {
+//    private static void showGroups(Matcher matcher) {
+//
+//        while (matcher.find()) {
+//            System.out.println("----------------------------");
+//            for (int i = 1; i < matcher.groupCount(); i++) {
+//                System.out.println("Group " + i + ": " + matcher.group(i));
+//            }
+//        }
+//    }
 
-        while (matcher.find()) {
-            System.out.println("----------------------------");
-            for (int i = 1; i < matcher.groupCount(); i++) {
-                System.out.println("Group " + i + ": " + matcher.group(i));
-            }
-        }
-    }
+
+//    protected static String styleSheet =
+//            "graph {" +
+//                    "   fill-color: white;"+
+//                    "}" +
+//
+//
+//            "node {" +
+//                    "   shape: line;" +
+//                    "   text-size: 15px;" +
+//                    "   shape: circle;" +
+//                    "   fill-color: green;" +
+//                    "   size: 20px;" +
+//                    "   text-alignment: center;" +
+//                    "   stroke-mode: plain;" +
+//                    "   stroke-color: yellow;" +
+//
+//                    "}" +
+//                    "node.marked {" +
+//                    "	fill-color: red;" +
+//                    "}" +
+//
+//   " edge {" +
+//        "shape: line;" +
+//       " stroke-mode: plain;"+
+//        "text-color: white;"+
+//        "text-size: 15;"+
+//        "fill-color: blue;"+
+//        "text-background-mode: rounded-box;"+
+//        "text-background-color: red;"+
+//    "}"+
+//
+//    "edge.marked {"+
+//       " fill-color: red;"+
+//    "}";
+
+    protected static String styleSheet =
+            "node {" +
+                    "	fill-color: black;" +
+                    "text-size: 20px;" +
+                    "shape: circle;" +
+                    "fill-color: red;"+
+                    "size: 20px;" +
+
+                    "}" +
+                    "node.marked {" +
+                    "	text-size: 20px;" +
+                    "	text-alignment: along;" +
+                    "	text-background-mode: rounded-box;" +
+                    "	stroke-mode: plain;" +
+                    "}";
 
 }
