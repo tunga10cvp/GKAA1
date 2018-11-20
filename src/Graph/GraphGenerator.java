8c5a1271cdbd8f8ff9f6ab972ed30a8327e6ae15
tungcr7 <tunga10cvp@gmail.com>
2018-11-20 15:25:19 +0100

    Teste für GraphGenerator

--- src/Graph/GraphGenerator.java
+++ src/Graph/GraphGenerator.java
@@ -15,23 +15,38 @@ public class GraphGenerator {
      * @param weightLimit   Limit bis wohin die Kantengewichte gehen
      * @return              Graph mit Vorgaben
      */
-    public static Graph generateGraph(String name, int nodeNum, int edgeNum, int weightLimit){
+    public static Graph generateGraph(String name, int nodeNum, int edgeNum, int weightLimit) throws Exception {
 
         Graph generatedGraph = new MultiGraph(name);
 
-        //generiere nodeNum Knoten
-        for(int i = 0;i<nodeNum;i++){
-            generatedGraph.addNode(String.valueOf(i));
+        // negative Anzahl
+        if (nodeNum < 0 || edgeNum < 0){
+            System.out.println("Kein Graph!!!");
         }
 
-        //generiere edgeNum Kanten
-        //Start- und Zielknoten werden dabei zufällig bestimmt.
-        for(int i = 0;i<edgeNum;i++){
-            String edgeId = String.valueOf(i);
-            generatedGraph.addEdge(edgeId, String.valueOf(Math.round(Math.floor(Math.random() * nodeNum))), String.valueOf(Math.round(Math.floor(Math.random() * nodeNum))), true);
-            generatedGraph.getEdge(edgeId).setAttribute("weight", Math.random() * weightLimit);
+        //keine Knoten aber trotzdem Kanten eingeben
+        else if(nodeNum <= 0 && edgeNum > 0){
+
+            throw new IllegalArgumentException( "Anzahl der Knoten muss größer als Anzahl der Kanten");
         }
 
+        else {
+            //generiere nodeNum Knoten
+            for (int i = 0; i < nodeNum; i++) {
+                generatedGraph.addNode(String.valueOf(i));
+
+            }
+            System.out.println(generatedGraph.getNode(1).getId());
+            //generiere edgeNum Kanten
+            //Start- und Zielknoten werden dabei zufällig bestimmt.
+            for (int i = 0; i < edgeNum; i++) {
+                String edgeId = String.valueOf(i);
+                generatedGraph.addEdge(edgeId, String.valueOf(Math.round(Math.floor(Math.random() * nodeNum))), String.valueOf(Math.round(Math.floor(Math.random() * nodeNum))), true);
+                generatedGraph.getEdge(edgeId).setAttribute("weight", Math.floor(Math.random() * 100) * weightLimit);
+                // nodeNum als Name der Knote setzen
+                generatedGraph.getEachNode().forEach(node -> node.addAttribute("ui.label", node.getId()));
+            }
+        }
         return generatedGraph;
     }
 }
