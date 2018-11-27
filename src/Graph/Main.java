52d676f4bbd31d88c2c65ea7eb43db535b239ee8
tungcr7 <tunga10cvp@gmail.com>
2018-11-27 15:21:39 +0100

    Klasse Main wo alle aufgerufen werden

--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -6,51 +6,44 @@ import org.graphstream.graph.Node;
 import java.util.ArrayList;
 import java.util.List;
 
+import static Graph.GraphAlgorithms.traverseWithBFS;
 import static Graph.ReadFile.readFile;
 
 public class Main {
 
     public static void main(final String[] args) throws InterruptedException {
-        String filename = "gka-Dateien/graph03.gka";
+        String filename = "gka-Dateien/negativeGRaph03.gka";
         Graph graph = readFile(filename);
 
         /**
          * BFS Algorithms aufrufen
          */
 
-//        List<Node> bfsReturn = traverseWithBFS(graph, graph.getNode("Kiel"), graph.getNode("Husum"));
-//
-//        for (Node node : bfsReturn){
-//            node.addAttribute("ui.style", " fill-color: red;");
-//            //testGraph.addAttribute("ui.stylesheet", "edge { fill-color: red; }");
-//        }
-//
-//        for(int i = 0; i < bfsReturn.size() - 1; i++){
-//            // System.out.println(bfsReturn.get(i)+bfsReturn.get(i+1));
-//            String node1 = String.valueOf(bfsReturn.get(i));
-//            String node2 = String.valueOf(bfsReturn.get(i+1));
-//            String edgeId = String.valueOf(i);
-//
-//            System.out.println(node1);
-//            System.out.println(node2);
-//
-//
-//        }
-//
-//        System.out.println(bfsReturn);
+        List<Node> bfsReturn = traverseWithBFS(graph, graph.getNode("Kiel"), graph.getNode("Celle"));
+
+        for (int i = 0; i < bfsReturn.size(); i++) {
+            bfsReturn.get(i).addAttribute("ui.style", " fill-color: red;");
+        }
+
+        for (int i = 0; i < bfsReturn.size() - 1; i++) {
+            bfsReturn.get(i).getEdgeToward(bfsReturn.get(i + 1)).addAttribute("ui.style", " fill-color: red;");
+        }
+        graph.display();
+
+        System.out.println(bfsReturn);
 
         /**
          * Graph Generator aufrufen
          */
-        Graph newGraph = null;
-        try {
-            newGraph = GraphGenerator.generateGraph("test1", 20, 50, 10);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        newGraph.addAttribute("ui.stylesheet", styleSheet);
-        newGraph.display();
-        System.out.println(FloydWarshalAlgorithm.shortestPathsWithFloydWarshal(newGraph, newGraph.getNode("0"),newGraph.getNode("1")));
+//        Graph newGraph = null;
+//        try {
+//            newGraph = GraphGenerator.generateGraph("test1", 20, 50, 10);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        newGraph.addAttribute("ui.stylesheet", styleSheet);
+//        newGraph.display();
+//        System.out.println(FloydWarshalAlgorithm.shortestPathsWithFloydWarshal(newGraph, newGraph.getNode("0"),newGraph.getNode("1")));
 
 
         /**
