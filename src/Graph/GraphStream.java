299ff7de5f3c918dc12e67f2af318d15ae9e0ca9
tungcr7 <tunga10cvp@gmail.com>
2019-01-05 17:52:33 +0100

    Test mit GraphStream über Mehrfachkanten

--- src/Graph/GraphStream.java
+++ src/Graph/GraphStream.java
@@ -1,10 +1,14 @@
 package Graph;
 
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.graph.implementations.SingleGraph;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class GraphStream {
     public static void main(String args[]) {
@@ -12,7 +16,7 @@ public class GraphStream {
     }
 
     public GraphStream() {
-        Graph graph = new SingleGraph("tutorial 1");
+        Graph graph = new MultiGraph("tutorial 1");
 
 //        SpriteManager sman = new SpriteManager(graph);
 //
@@ -51,15 +55,60 @@ public class GraphStream {
 
         explore(graph.getNode("A"));*/
 
-        Graph newGraph = null;
-        try {
-           newGraph = GraphGenerator.generateGraph("test1", 8, 15, 10);
-        } catch (Exception e) {
-            e.printStackTrace();
+//        Graph newGraph = null;
+//        try {
+//           newGraph = GraphGenerator.generateGraph("test1", 8, 15, 10);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        newGraph.addAttribute("ui.stylesheet", styleSheet);
+//        newGraph.display();
+//        System.out.println(FloydWarshalAlgorithm.shortestPathsWithFloydWarshal(newGraph, newGraph.getNode("0"),newGraph.getNode("1")));
+
+        graph.addNode("x1");
+        graph.addNode("x2");
+
+        graph.addEdge("1", "x1", "x2", true);
+        graph.getEdge("1").setAttribute("ui.label", 3);
+
+        graph.addEdge("2", "x1", "x2", true);
+        graph.getEdge("2").setAttribute("ui.label", 10);
+
+        graph.addEdge("3", "x1", "x2", true);
+        graph.getEdge("3").setAttribute("ui.label", 110);
+
+        int V = graph.getNodeCount();
+        int residualGraphMatrix[][] = new int[V][V];
+
+        List<Node> nodes = new ArrayList<>();
+
+        for (Node node : graph.getEachNode()) {
+            nodes.add(node);
         }
-        newGraph.addAttribute("ui.stylesheet", styleSheet);
-        newGraph.display();
-        System.out.println(FloydWarshalAlgorithm.shortestPathsWithFloydWarshal(newGraph, newGraph.getNode("0"),newGraph.getNode("1")));
+
+        // Add up all edge's weights from nodes i to j in the matrix, if not connected -> 0
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+
+                for (Edge leavingEdge : nodes.get(i).getEachLeavingEdge()) {
+                    if (nodes.get(i).hasEdgeToward(nodes.get(j))) {
+                        if (residualGraphMatrix[i][j] > 0)
+                            residualGraphMatrix[i][j] += (int)leavingEdge.getAttribute("ui.label");
+                        else
+                            residualGraphMatrix[i][j] = (int)leavingEdge.getAttribute("ui.label");
+                    }
+                    else
+                        residualGraphMatrix[i][j] = 0;
+                }
+            }
+        }
+
+        System.out.println(residualGraphMatrix[0][1]);
+        System.out.println(residualGraphMatrix[1][0]);
+
+        graph.addAttribute("ui.stylesheet", styleSheet);
+        graph.display();
+
     }
 
     public void explore(Node source) {
@@ -76,7 +125,6 @@ public class GraphStream {
     protected void sleep() {
         try { Thread.sleep(1000); } catch (Exception e) {}
     }
-
 
     // Aussehen für die Graphen
     protected static String styleSheet =
